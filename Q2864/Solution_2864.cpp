86e6ae105a8c3478beabb2b306a7dea0a88db4dd
Fanindra Nayak <fanindranayak@gmail.com>
2023-09-24 21:55:01 +0530

    Q2864 Leetcode 24/09/2023 question

---
+++ Q2864/Solution_2864.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string maxBinary(string &s)
+    {
+        string ans = "";
+
+        int count = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1')
+                count++;
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (count > 1)
+            {
+                ans += "1";
+                count--;
+            }
+            else
+            {
+                ans += "0";
+            }
+        }
+
+        if (count == 1)
+            ans += "1";
+        else
+            ans += "0";
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string s = "010";
+    string ans = sol.maxBinary(s);
+    cout << ans;
+    return 0;
+}
